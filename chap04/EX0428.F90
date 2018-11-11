05709c96976ddc5013c9aeac900a9489b06668cc
pam-phy <pam.phys.th@gmail.com>
2018-11-11 23:37:34 +0800

    Ex

---
+++ chap04/EX0428.F90
@@ -0,0 +1,7 @@
+program ex0429
+  implicit none
+!  real pi
+!  parameter(pi=3.14159)
+  real, parameter :: pi=3.14159
+  write(*,"(F4.2)") sin(pi/6)
+end
---
+++ chap04/EX0430.F90
@@ -0,0 +1,7 @@
+program ex0430
+  integer :: a = 1
+  real :: b = 2.0
+  complex :: c = (1.0,2.0)
+  character(len=20) :: str = "Fortran 90"
+  write(*,*) a, b, c, str
+end
